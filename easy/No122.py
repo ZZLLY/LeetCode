0d9ff835fe8b7504f2e9f5deb13f6aaa219b95f7
ZZLLY <472377522@qq.com>
2019-05-14 09:45:44 +0800

    day11

---
+++ easy/No122.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxProfit(self, prices: list) -> int:
+        # 题意: 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+        #      设计一个算法来计算你所能获取的最大利润。你可以尽可能地完成更多的交易（多次买卖一支股票）。
+        # 限制: 你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）
+        # 思路: 当前天与前一天的差值, 如果大于0就加入到res中, 否则加0
+        res = 0
+        for i in range(1, len(prices)):
+            res += prices[i]-prices[i-1] if prices[i]-prices[i-1] > 0 else 0
+        return res
